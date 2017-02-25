54971b13e16bdc319cae56b50c73b4f6ebdf0071
zarlex <alex.zarges@online.de>
2017-02-25 17:17:50 +0100

    add a footer with copy right and version number

--- src/app/modules/main/controllers/main.controller.js
+++ src/app/modules/main/controllers/main.controller.js
@@ -1,8 +1,22 @@
 angular.module('mwPortal.Main')
-  .controller('MainController', function (i18n) {
+  .controller('MainController', function (i18n, config) {
+    var version = '';
     this.locales = i18n.getLocales();
 
     this.setLocale = function (id) {
       i18n.setLocale(id);
+      window.localStorage.setItem('locale', id);
     };
+
+    this.getCurrentYear = function () {
+      return (new Date()).getFullYear();
+    };
+
+    this.getAppversion = function () {
+      return version;
+    };
+
+    config.getConfig().then(function (config) {
+      version = config.version;
+    });
   });
