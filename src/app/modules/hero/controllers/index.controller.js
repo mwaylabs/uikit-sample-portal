8824035adf07a39463dc0bbf4c34742b4f721db3
zarlex <alex.zarges@online.de>
2017-03-01 12:58:45 +0100

    add comments

--- src/app/modules/hero/controllers/index.controller.js
+++ src/app/modules/hero/controllers/index.controller.js
@@ -1,13 +1,18 @@
 angular.module('mwPortal.Hero')
   .controller('HeroIndexController', function (HeroDeleteConfirmModal, heroes) {
+    // Create an instance of the delete confirm modal
     var heroDeleteConfirmModal = new HeroDeleteConfirmModal();
 
     this.heroes = heroes;
 
     this.showDeleteConfirmModal = function(){
+      // prepare the modal and set all attributes that are needed in the modal
       heroDeleteConfirmModal.setScopeAttributes({
+        // this attribute will be available in the modal controller
         selected: this.heroes.selectable.getSelected()
       });
+
+      // show the modal
       heroDeleteConfirmModal.show();
     };
   })
