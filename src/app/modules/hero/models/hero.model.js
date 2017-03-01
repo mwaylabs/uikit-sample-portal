485aa68bf01fcda5341a9d401c8b196ccddf5438
zarlex <alex.zarges@online.de>
2017-03-01 19:22:30 +0100

    add nested attribute superPowers for all the super powers the hero have

--- src/app/modules/hero/models/hero.model.js
+++ src/app/modules/hero/models/hero.model.js
@@ -1,6 +1,14 @@
 angular.module('mwPortal.Main')
-  .service('HeroModel', function (MwPortalModel) {
+  .service('HeroModel', function (MwPortalModel, SuperPowersCollection) {
     return MwPortalModel.extend({
-      endpoint: '/heroes'
+      endpoint: '/heroes',
+      // This is a nested attribute. That means when the attribute superPowers is set with an array
+      // it is automatically resolved into the defined collection
+      // So when you call model.get('superPowers') you will get a collection instead of an array
+      nested: function () {
+        return {
+          superPowers: SuperPowersCollection
+        };
+      }
     });
   });
