4367a8b8a9f4c9eb31db065ae98f893b6d86132a
zarlex <alex.zarges@online.de>
2017-03-07 19:35:56 +0100

    fix typo

--- src/app/modules/super-power/super-power.js
+++ src/app/modules/super-power/super-power.js
@@ -8,7 +8,7 @@ angular.module('mwPortal.SuperPower', [])
   .config(function (ResponseToastHandlerProvider) {
     // Display toast when a POST request on the route was successful (item has been created on server)
     ResponseToastHandlerProvider.registerSuccessToast(
-      '/api/super-powers', // /api/heroes route
+      '/api/super-powers', // /api/super-powers route
       {
         singular: 'superPower.toasts.serverResponses.success.created.singular',
         plural: 'superPower.toasts.serverResponses.success.created.plural'
@@ -19,7 +19,7 @@ angular.module('mwPortal.SuperPower', [])
 
     // Display toast when a PUT request on the route was successful (item has been updated on server)
     ResponseToastHandlerProvider.registerSuccessToast(
-      '/api/super-powers/*', // /api/heroes/{id} route
+      '/api/super-powers/*', // /api/super-powers/{id} route
       {
         singular: 'superPower.toasts.serverResponses.success.updated.singular',
         plural: 'superPower.toasts.serverResponses.success.updated.plural'
@@ -30,7 +30,7 @@ angular.module('mwPortal.SuperPower', [])
 
     // Display toast when a DELETE request on the route was successful (item has been deleted from server)
     ResponseToastHandlerProvider.registerSuccessToast(
-      '/api/super-powers/*', // /api/heroes/{id} route
+      '/api/super-powers/*', // /api/super-powers/{id} route
       {
         singular: 'superPower.toasts.serverResponses.success.deleted.singular',
         plural: 'superPower.toasts.serverResponses.success.deleted.plural'
