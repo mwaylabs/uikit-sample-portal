06a0235adf5ce73ff08bd65f0397f83942f2b315
zarlex <alex.zarges@online.de>
2017-03-10 15:58:57 +0100

    remove superPowers because not needed anymore

--- src/app/modules/hero/controllers/form.controller.js
+++ src/app/modules/hero/controllers/form.controller.js
@@ -1,7 +1,7 @@
 angular.module('mwPortal.Hero')
-  .controller('HeroFormController', function ($location, SuperPowersCollection, hero) {
+  .controller('HeroFormController', function ($location, hero) {
     this.hero = hero;
-    this.superPowers = new SuperPowersCollection();
+
     this.goBack = function(){
       $location.path('/heroes');
     };
@@ -15,10 +15,6 @@ angular.module('mwPortal.Hero')
     this.cancel = function(){
       this.goBack();
     };
-
-    // you can also fetch the superPowers in the preresolver, but as it is not super important for the view
-    // we can also do it here to speed up the time the user has to view until the view is displayed
-    this.superPowers.fetch();
   })
 
   .constant('HeroFormControllerResolver', {
