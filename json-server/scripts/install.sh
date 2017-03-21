5b7ef85d4fcc880b944d40a6fafe5f48b1a87c89
zarlex <alex.zarges@online.de>
2017-03-21 11:54:09 +0100

    add echo

--- json-server/scripts/install.sh
+++ json-server/scripts/install.sh
@@ -2,17 +2,22 @@
 set -e
 
 # update instance
+echo "update instance"
 sudo apt-get update
 
+echo "installing essentials"
 sudo apt-get install build-essential libssl-dev
 
 # install nvm
+echo "installing NVM"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 source ~/.profile
 
 # install node
+echo "installing node"
 nvm install node
 
 # install pm2 module globaly
+echo "installing pm2"
 sudo npm install -g pm2
 pm2 update
