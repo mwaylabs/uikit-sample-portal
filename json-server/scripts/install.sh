b3506e870fb7f221ca77f8d57ff1804ff3054d97
zarlex <alex.zarges@online.de>
2017-03-21 14:46:06 +0100

    save into bash_rc

--- json-server/scripts/install.sh
+++ json-server/scripts/install.sh
@@ -13,6 +13,15 @@ echo "installing NVM"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+source "$NVM_DIR/nvm.sh"
+
+# save nvm env in bash rc
+hasNvmEnv=`grep "export NVM_DIR" ~/.bash_profile | cat`
+if [ -z "$hasNvmEnv" ]; then
+  echo "export NVM_DIR='$HOME/.nvm'" >> ~/.bash_profile
+  echo "[ -s '$NVM_DIR/nvm.sh' ] && \. '$NVM_DIR/nvm.sh'" >> ~/.bash_profile
+  echo "source '$NVM_DIR/nvm.sh'" >> ~/.bash_profile
+fi
 
 # install node
 echo "installing node"
