2c4f433454e5a19611b14470d2cae1763fb54f04
zarlex <alex.zarges@online.de>
2017-03-21 15:55:27 +0100

    move installation of node into ubuntu user scope

--- json-server/scripts/install.sh
+++ json-server/scripts/install.sh
@@ -7,20 +7,3 @@ sudo apt-get update
 
 echo "installing essentials"
 sudo apt-get install build-essential libssl-dev
-
-# install nvm
-echo "installing NVM"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
-
-# setup nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# install node
-echo "installing node"
-nvm install node
-
-# install pm2 module globaly
-echo "installing pm2"
-sudo npm install -g pm2
-pm2 update
--- json-server/scripts/post_install.sh
+++ json-server/scripts/post_install.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
+# install nvm
+echo "installing NVM"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+
+# setup nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+source "$NVM_DIR/nvm.sh"
+
+# install node
+echo "installing node"
+nvm install node
+
+# install pm2 module globaly
+echo "installing pm2"
+sudo npm install -g pm2
+pm2 update
+
 cd ~/mw-uikit
 npm install
 
@@ -15,11 +33,6 @@ if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
         sed -i "/export NODE_ENV=\b/c\export NODE_ENV=$DEPLOYMENT_GROUP_NAME" ~/.bash_profile
     fi
 fi
-
-# setup nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-source "$NVM_DIR/nvm.sh"
 
 # save nvm env in bash rc
 hasNvmEnv=`grep "export NVM_DIR" ~/.bash_profile | cat`
