c2dfdcb4bbc49e52b3eebab7ae7d16628fb917df
zarlex <alex.zarges@online.de>
2017-04-21 16:38:31 +0200

    add bridge to use different api url in production mode

--- src/app/modules/main/models/mw-portal.model.js
+++ src/app/modules/main/models/mw-portal.model.js
@@ -1,7 +1,13 @@
 angular.module('mwPortal.Main')
   .service('MwPortalModel', function () {
     return mwUI.Backbone.Model.extend({
-      baseUrl: '',
+      hostName: function(){
+        if(window.CONFIG.env === 'PRODUCTION'){
+          return 'http://52.59.202.190:3000';
+        } else {
+          return '';
+        }
+      },
       basePath: '/api',
       parse: function(rsp){
         return rsp.data || rsp;
