2630964c280daf4f2bb68a6cfdcbc2d02a24c5dc
zarlex <alex.zarges@online.de>
2017-04-21 17:21:43 +0200

    update run script

--- json-server/scripts/run.sh
+++ json-server/scripts/run.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
-if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
- export NODE_ENV=$DEPLOYMENT_GROUP_NAME
-fi
-
 cd ~/mw-uikit/app
-pm2 start json-server -n www -i 0
+pm2 start json-server.js -n www -i 0
